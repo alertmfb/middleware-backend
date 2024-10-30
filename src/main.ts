5d8802e76d268dffc0d63c1bddb067b76448c73f
Victor <victor.balogun@alertgroup.com.ng>
2024-10-30 10:15:55 +0100

    feat: added prod domain to allowed cors origins

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'https://middleware.staging.alertmfb.com.ng',
+      'https://middleware.alertmfb.com.ng',
     ],
   });
 
