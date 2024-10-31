8580db7682c0ba1f1eb50ed48c176d8f646ce011
Victor <victor.balogun@alertgroup.com.ng>
2024-10-31 13:58:33 +0100

    api key present

--- src/config/resend.ts
+++ src/config/resend.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
-import { ConfigService } from '@nestjs/config';
-
-const config = new ConfigService();
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
+// import { ConfigService } from '@nestjs/config';
+
+// const config = new ConfigService();
