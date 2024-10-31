b77324b27ef0e58cdc2264f8e2f0a4c46c792ac8
Victor <victor.balogun@alertgroup.com.ng>
2024-10-31 14:07:01 +0100

    fix up

--- src/config/resend.ts
+++ src/config/resend.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { Resend } from 'resend';
 
-const API_KEY = process.env.RESEND_API_KEY;
+// const API_KEY = process.env.RESEND_API_KEY;
 
-export const resend = new Resend(API_KEY);
+export const resend = new Resend('re_XbigChV6_KwhdiDG1ybsqhoKmG2oNe6KU');
 // import { ConfigService } from '@nestjs/config';
 
 // const config = new ConfigService();
