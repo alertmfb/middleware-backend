9d4b9994429a017d4817291ab34a1a34d186a1c5
Victor <victor.balogun@alertgroup.com.ng>
2024-11-28 13:23:35 +0100

    feat: user designations schema

---
+++ prisma/migrations/20241128122308_user_designation/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "designationId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Designation" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Designation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Designation_name_key" ON "Designation"("name");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_designationId_fkey" FOREIGN KEY ("designationId") REFERENCES "Designation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
