5a82ded1fa7c12684a6c61c9049ca6a245f5d019
Victor <victor.balogun@alertgroup.com.ng>
2024-11-28 02:05:51 +0100

    fix: merge conflict

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,60 @@
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Request, Response } from 'express';
+import { Public } from '../auth/metadata';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiExcludeController,
+  ApiExcludeEndpoint,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Roles } from 'src/decorators/roles.decorator';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { ROLES } from 'src/common/roles.enum';
+import { usersApiResponse } from './dto/users.dto';
+@Controller('users')
+// @ApiExcludeController()
+@ApiBearerAuth()
+@ApiTags('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Get('/')
+  @UseGuards(RolesGuard)
+  @Roles([ROLES.SUPER_ADMIN, ROLES.ADMIN])
+  @ApiResponse({ example: usersApiResponse })
+  async usersList() {
+    return this.userService.getUsers();
+  }
+  @Post('create')
+  @Public()
+  @ApiExcludeEndpoint()
+  async createUser(@Req() req: Request, @Res() res: Response) {
+    const token = req.query && (req.query['token'] as string);
+    const user = await this.userService.createUser(
+      req.body['email'],
+      'password',
+      token,
+    );
+    res.json(user);
+  }
+  @Post('backend/create')
+  @Public()
+  @ApiExcludeEndpoint()
+  async createUserFromBackend(@Req() req: Request, @Res() res: Response) {
+    const user = await this.userService.createUserFromBackend(
+      req.body['email'],
+      'password',
+      req.body['role'],
+    );
+    res.json(user);
+  }
+  @Public()
+  @ApiExcludeEndpoint()
+  @Post('tamper')
+  async tamperUser(@Req() req: Request, @Res() res: Response) {
+    res.json(await this.userService.tamper());
+  }
+}
