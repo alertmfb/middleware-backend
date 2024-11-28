ec8bc40901293fcee044748f8b861bb6823e412f
Victor <victor.balogun@alertgroup.com.ng>
2024-11-28 10:15:48 +0100

    deploy: cheeky 2

--- prisma/migrations/20241128075208_added_two_roles/migration.sql
+++ prisma/migrations/20241128075208_added_two_roles/migration.sql
@@ -9,6 +9,4 @@
 ALTER TYPE "ROLE" ADD VALUE 'SENIOR';
 ALTER TYPE "ROLE" ADD VALUE 'JUNIOR';
 
-UPDATE _prisma_migrations
-SET rolled_back_at = NOW()
-WHERE migration_name = '20241128084845_removed_roles_member_admin';
+DELETE FROM _prisma_migrations WHERE migration_name = '20241128084845_removed_roles_member_admin';
