f628181a222512ffe9d52eb3130961e900515d22
Victor <victor.balogun@alertgroup.com.ng>
2024-11-30 08:00:54 +0100

    feat: product endpoints

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -23,19 +23,59 @@ export class ProductsService {
 
   async createProduct(payload: CreateProduct) {
     try {
-      // const product = await this.prisma.products.
+      const product = await this.prisma.product.create({
+        data: payload,
+        select: { name: true },
+      });
+
+      return {
+        success: true,
+        messgae: product.name,
+      };
     } catch (error) {
       throw new BadRequestException();
     }
   }
   async enableProduct(payload: EnableProduct) {
     try {
+      const product = await this.prisma.product.update({
+        where: {
+          id: payload.id,
+        },
+        data: {
+          isEnabled: true,
+        },
+        select: {
+          name: true,
+        },
+      });
+
+      return {
+        success: true,
+        message: `${product.name} has been enabled`,
+      };
     } catch (error) {
       throw new BadRequestException();
     }
   }
   async disableProduct(payload: DisableProduct) {
     try {
+      const product = await this.prisma.product.update({
+        where: {
+          id: payload.id,
+        },
+        data: {
+          isEnabled: false,
+        },
+        select: {
+          name: true,
+        },
+      });
+
+      return {
+        success: true,
+        message: `${product.name} has been disabled`,
+      };
     } catch (error) {
       throw new BadRequestException();
     }
