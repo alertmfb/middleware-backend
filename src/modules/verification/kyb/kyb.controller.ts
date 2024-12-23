5a161938b834de362da3d5cb912d9ac40ef0e00a
Victor <balogunv50@gmail.com>
2024-12-23 16:18:10 +0100

    feat(docs): updated responses on kyb endpoints

--- src/modules/verification/kyb/kyb.controller.ts
+++ src/modules/verification/kyb/kyb.controller.ts
@@ -3,6 +3,11 @@ import { KybService } from './kyb.service';
 import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AdvancedType, CompanyType } from './dto/kyb.dto';
 import { Public } from 'src/modules/auth/metadata';
+import {
+  cacAdvancedResponse,
+  cacBasicResponse,
+  tinResponse,
+} from './dto/kyb.response';
 
 @Public()
 @ApiTags('verification')
@@ -11,11 +16,13 @@ export class KybController {
   constructor(private readonly kybService: KybService) {}
 
   @Get('tin')
+  @ApiResponse({ example: tinResponse })
   async validateTin(@Query('tin') tin: string) {
     return await this.kybService.validateTin(tin);
   }
 
   @Get('cac/basic')
+  @ApiResponse({ example: cacBasicResponse })
   @ApiQuery({ name: 'companyType', enum: CompanyType })
   async cacBasic(
     @Query('rcNumber') rcNumber: string,
@@ -25,6 +32,7 @@ export class KybController {
   }
 
   @Get('cac/advanced')
+  @ApiResponse({ example: cacAdvancedResponse })
   @ApiQuery({ name: 'type', enum: AdvancedType })
   async cacAdvanced(
     @Query('rcNumber') rcNumber: string,
