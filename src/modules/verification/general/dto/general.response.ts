51f20d6f56b895b3292b1dc283e6d3af7e67a928
Victor <balogunv50@gmail.com>
2024-12-23 16:08:15 +0100

    feat(docs): identity and general responses updated

---
+++ src/modules/verification/general/dto/general.response.ts
@@ -0,0 +1,24 @@
+export const banksResponse = {
+  entity: [
+    {
+      name: 'Parallex MFB',
+      code: '015',
+    },
+    {
+      name: 'Globus Bank',
+      code: '00103',
+    },
+    {
+      name: 'NOW NOW',
+      code: '075',
+    },
+    {
+      name: 'ALAT by WEMA',
+      code: '035A',
+    },
+    {
+      name: 'Eyowo',
+      code: '50126',
+    },
+  ],
+};
--- src/modules/verification/identity/identity.controller.ts
+++ src/modules/verification/identity/identity.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { IdentityService } from './identity.service';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import {
   LivelinessCheck,
   VerifyBvnSelfie,
   VerifyNinSelfie,
 } from './dto/identity.dto';
 import { Public } from 'src/modules/auth/metadata';
+import {
+  bvnSelfieResponse,
+  livelinessResponse,
+  ninSelfieResponse,
+} from './dto/identity.response';
 
 @Public()
 @ApiTags('verification')
@@ -16,18 +21,21 @@ export class IdentityController {
 
   @Post('nin/selfie')
   @ApiBody({ type: VerifyNinSelfie })
+  @ApiResponse({ example: ninSelfieResponse })
   async verifyNinSelfie(@Body() payload: VerifyNinSelfie) {
     return this.identityService.verifyNin(payload);
   }
 
   @Post('bvn/selfie')
   @ApiBody({ type: VerifyBvnSelfie })
+  @ApiResponse({ example: bvnSelfieResponse })
   async verifyBvnSelfie(@Body() payload: VerifyBvnSelfie) {
     return this.identityService.verifyBvn(payload);
   }
 
-  @Post('lineliness')
+  @Post('liveliness')
   @ApiBody({ type: LivelinessCheck })
+  @ApiResponse({ example: livelinessResponse })
   async liveliness(@Body() payload: LivelinessCheck) {
     return this.identityService.livelinessCheck(payload);
   }
