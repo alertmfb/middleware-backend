8905390ac7b710403d7f18db63ed53522a0c7ba6
Victor <balogunv50@gmail.com>
2024-12-24 08:43:47 +0100

    feat: added core transactions response

---
+++ src/modules/core-banking/transactions/dto/transactions.response.ts
@@ -0,0 +1,25 @@
+export const interBankTransferResponse = {
+  Status: 'Successful',
+  StatusDescription: null,
+  ReferenceID: 0,
+  UniqueIdentifier: '020067152410012328260000000000000000000000',
+  IsSuccessFul: true,
+  ResponseMessage: null,
+  ResponseCode: '00',
+  Reference: 'TF24107924',
+  SessionID: null,
+  RequestStatus: true,
+  ResponseDescription: null,
+  ResponseStatus: 'Successful',
+};
+
+export const nameEnquiry = {
+  Name: 'AKINTOMIWA AKINYEMI',
+  BVN: '22233344455',
+  KYC: '3',
+  IsSuccessful: true,
+  SessionID: '',
+  ResponseMessage: 'Successful',
+  DefaultGateWay: 'ISW',
+  InstitutionCode: null,
+};
