eff8e18124f51325f54212ca5bdd85703ad695d7
Victor <balogunv50@gmail.com>
2024-12-31 03:42:54 +0100

    chore: removed logger import

--- src/modules/virtual-accounts/transactions/transactions.service.ts
+++ src/modules/virtual-accounts/transactions/transactions.service.ts
@@ -3,7 +3,6 @@ import {
   HttpException,
   Inject,
   Injectable,
-  Logger,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { BANKONE_SERVICE, BANKONE_TSQ_SERVICE } from '../../bankone/constants';
