0e6765e0dd23c77eaf67a3005125225cfe564b8c
Victor <balogunv50@gmail.com>
2024-12-31 03:43:25 +0100

    chore: removed response import from express

--- src/modules/virtual-accounts/transactions/transactions.service.ts
+++ src/modules/virtual-accounts/transactions/transactions.service.ts
@@ -8,7 +8,6 @@ import { ConfigService } from '@nestjs/config';
 import { BANKONE_SERVICE, BANKONE_TSQ_SERVICE } from '../../bankone/constants';
 import { HttpService } from '@nestjs/axios';
 import { AxiosError } from 'axios';
-import { response } from 'express';
 import {
   AccountTransaction,
   InterBankTransfer,
