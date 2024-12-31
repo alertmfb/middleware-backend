b35a725e5540b7823852b2b479a131619f15aab8
Victor <balogunv50@gmail.com>
2024-12-31 15:13:58 +0100

    feat: created swaggerc config

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,30 @@
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+export function useSwaggerConfig(
+  app: NestExpressApplication<
+    Server<typeof IncomingMessage, typeof ServerResponse>
+  >,
+) {
+  const docsConfig = new DocumentBuilder()
+    .setTitle('Middleware')
+    .setDescription('Middleware api reference')
+    .addBearerAuth()
+    .addServer('http://localhost:3000', 'local')
+    .addServer('https://api-middleware-staging.alertmfb.com.ng', 'sandbox')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docsConfig, {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  });
+
+  SwaggerModule.setup('reference', app, document, {
+    jsonDocumentUrl: 'reference/json',
+    swaggerUiEnabled: true,
+  });
+}
