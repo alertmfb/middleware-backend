984b1d774cb8a68a883b82fe2aa27f2709bf631b
Victor <balogunv50@gmail.com>
2025-01-01 18:48:33 +0100

    chore: removed unused api body decorator

--- src/modules/pos/pos.controller.ts
+++ src/modules/pos/pos.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { PosService } from './pos.service';
-import { ApiBearerAuth, ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
 import { Public } from '../auth/metadata';
 import { CreateVirtualSubAccount } from '../virtual-accounts/accounts/dto/accounts.dto';
 import { AccountsService } from '../virtual-accounts/accounts/accounts.service';
