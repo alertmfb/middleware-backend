49adc71269b3a8fd6c475325fc8b192d4ffa1591
Victor <balogunv50@gmail.com>
2025-01-02 15:55:31 +0100

    feat: seperated config methods

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,18 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { useSwaggerConfig } from './config/swagger.config';
+import { useAppConfig } from './config/app.config';
 
 async function bootstrap() {
   const config = new ConfigService();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    credentials: true,
-    origin: [
-      'http://localhost:5173',
-      'https://middleware.staging.alertmfb.com.ng',
-    ],
-  });
-
-  app.disable('x-powered-by');
-
-  app.setGlobalPrefix(config.get('GLOBAL_PREFIX'));
-
-  const docsConfig = new DocumentBuilder()
-    .setTitle('Middleware')
-    .setDescription('Middleware api reference')
-    .addBearerAuth()
-    .addServer('http://localhost:3000', 'local')
-    .addServer('https://api-middleware-staging.alertmfb.com.ng', 'sandbox')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, docsConfig, {
-    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
-  });
-
-  SwaggerModule.setup('reference', app, document, {
-    jsonDocumentUrl: 'reference/json',
-    swaggerUiEnabled: true,
-  });
+  useAppConfig(app);
+  useSwaggerConfig(app);
 
   const redisMicroservice =
     await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
