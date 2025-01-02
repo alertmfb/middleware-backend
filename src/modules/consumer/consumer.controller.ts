b2682562af47e3d7abcd5bee14c06c9ff0e33d20
Victor <balogunv50@gmail.com>
2025-01-02 08:21:32 +0100

    feat: endpoint for getting customers

--- src/modules/consumer/consumer.controller.ts
+++ src/modules/consumer/consumer.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ConsumerService } from './consumer.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Public } from '../auth/metadata';
+import { AccountsService } from '../core-banking/accounts/accounts.service';
 
+@ApiTags('consumer-banking')
+@ApiBearerAuth()
+@Public()
 @Controller('consumer')
 export class ConsumerController {
-  constructor(private readonly consumerService: ConsumerService) {}
+  constructor(
+    private readonly consumerService: ConsumerService,
+    private readonly accountsService: AccountsService,
+  ) {}
+
+  @Get('customers')
+  async getCustomers() {
+    return this.consumerService.getCustomers();
+  }
 }
--- src/modules/consumer/consumer.service.ts
+++ src/modules/consumer/consumer.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
 
 @Injectable()
-export class ConsumerService {}
+export class ConsumerService {
+  async getCustomers() {
+    // TODO: Fetch Businesses from business backend
+    let customers = [];
+
+    for (let i = 0; i < 20; i++) {
+      customers.push({
+        id: faker.string.uuid(),
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        lastActive: faker.date.anytime(),
+        dateRegistered: faker.date.anytime(),
+        kycStatus: true,
+      });
+    }
+
+    return customers;
+  }
+}
