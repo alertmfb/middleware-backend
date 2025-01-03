6e9db4af9c7969a8d4c92184f3ec39b37983a835
Victor <balogunv50@gmail.com>
2025-01-03 10:28:53 +0100

    feat: added client module under imports

--- src/modules/pos/pos.module.ts
+++ src/modules/pos/pos.module.ts
@@ -3,9 +3,28 @@ import { PosService } from './pos.service';
 import { PosController } from './pos.controller';
 import { AccountsService } from '../virtual-accounts/accounts/accounts.service';
 import { BankoneHttpModule } from '../bankone/bankone.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ACCOUNT_EVENTS } from '../virtual-accounts/events/constants';
 
 @Module({
-  imports: [BankoneHttpModule],
+  imports: [
+    BankoneHttpModule,
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        name: ACCOUNT_EVENTS,
+        useFactory: async (config: ConfigService) => ({
+          transport: Transport.REDIS,
+          options: {
+            host: config.get('REDIS_HOST') || 'middleware-redis',
+            port: config.get('REDIS_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [PosController],
   providers: [PosService, AccountsService],
 })
