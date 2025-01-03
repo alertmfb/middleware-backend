1d605292a3251fe3b010bfed77a46e04d4671b1b
Victor <balogunv50@gmail.com>
2025-01-03 13:16:06 +0100

    chore: removed error import

--- src/modules/virtual-accounts/events/events.service.ts
+++ src/modules/virtual-accounts/events/events.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { serviceLogger } from 'src/config/logger.config';
 import { PrismaService } from 'src/config/prisma.service';
 import { SaveAccountEvent } from './dto/events.dto';
-import { error } from 'console';
 
 @Injectable()
 export class EventsService {
