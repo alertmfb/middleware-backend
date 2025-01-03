e2ad034121218f5130a15f8cc3e46c3b9b698c50
Victor <balogunv50@gmail.com>
2025-01-03 12:40:54 +0100

    feat: create account on event trigger

--- src/modules/virtual-accounts/events/events.service.ts
+++ src/modules/virtual-accounts/events/events.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { serviceLogger } from 'src/config/logger.config';
 import { PrismaService } from 'src/config/prisma.service';
 import { SaveAccountEvent } from './dto/events.dto';
+import { error } from 'console';
 
 @Injectable()
 export class EventsService {
@@ -9,7 +10,22 @@ export class EventsService {
 
   async saveAccount(payload: SaveAccountEvent) {
     try {
-      serviceLogger.info(payload);
-    } catch (error) {}
+      const account = await this.prisma.virtualAccount.create({
+        data: {
+          accountName: payload.Name,
+          accountNo: payload.Nuban,
+          productCode: payload.ProductCode,
+        },
+      });
+
+      if (account.id) {
+        serviceLogger.info(`Account Created: id: ${account.id}`);
+      }
+    } catch (error) {
+      serviceLogger.error(error, {
+        class: EventsService.name,
+        method: this.saveAccount.name,
+      });
+    }
   }
 }
