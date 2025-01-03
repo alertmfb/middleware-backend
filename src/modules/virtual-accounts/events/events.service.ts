20c449bacd046884f91f5c596300e56a56b08ae5
Victor <balogunv50@gmail.com>
2025-01-03 10:30:58 +0100

    feat: events module

--- src/modules/virtual-accounts/events/events.service.ts
+++ src/modules/virtual-accounts/events/events.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { serviceLogger } from 'src/config/logger.config';
+import { PrismaService } from 'src/config/prisma.service';
+import { SaveAccountEvent } from './dto/events.dto';
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+  constructor(private prisma: PrismaService) {}
+
+  async saveAccount(payload: SaveAccountEvent) {
+    try {
+      serviceLogger.info(payload);
+    } catch (error) {}
+  }
+}
